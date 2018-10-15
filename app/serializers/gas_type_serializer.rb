dff74dbb3db5fe38e6622d676cf186f6d5d7c64b
Jorge Burgos <jburgosk8@gmail.com>
2018-10-14 19:32:19 -0600

    Adds serializers for other models

---
+++ app/serializers/gas_type_serializer.rb
@@ -0,0 +1,3 @@
+class GasTypeSerializer < ActiveModel::Serializer
+  attributes :id, :name, :identifier
+end
---
+++ app/serializers/zone_serializer.rb
@@ -0,0 +1,3 @@
+class ZoneSerializer < ActiveModel::Serializer
+  attributes :id, :name, :identifier
+end
